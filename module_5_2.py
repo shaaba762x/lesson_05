1623181437d23f0a2116a38520cc066f36a7d360
shaaba762x <shaaba777@gmail.com>
2024-12-07 17:56:42 +0300

    м5 з2

---
+++ module_5_2.py
@@ -0,0 +1,36 @@
+class Hause:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor <= 0:
+           print(f'в {self.name} подвала нет и мы никуда не едем.')
+        elif self.new_floor <= self.number_of_floors:
+            for floor in range(1, self.new_floor+1):
+                print(floor)
+                if floor == self.number_of_floors:
+                    print(f'Мы поднялись на {self.new_floor} этаж.')
+                    return
+        else:
+            print(f'В {self.name} всего {self.number_of_floors} этажей. Выше не уехать.')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        print(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}.")
+        return self.name
+h1 = Hause('ТЦ "Вавилон"', 122)
+h2 = Hause('ТЦ "Башня"', 5)
+# print(h1.name,'Этажей -',h1.number_of_floors)
+# print(h2.name,'Этажей -',h2.number_of_floors)
+_a = (int(input(f'Введите номер этажа на который хотите подняться: ',)))
+h1.go_to(_a)
+h2.go_to(_a)
+
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
